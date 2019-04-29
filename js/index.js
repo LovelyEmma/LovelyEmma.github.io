593d53648bfdafcbcc982563ec09c7696dae2f7e
Emma <lovelyemmali@outlook.com>
2019-04-29 23:18:40 +1200

    version 2

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  
-  var backgroundImg = [
-    'http://sfwallpaper.com/images/natural-wallpapers-7.jpg',
-'https://images.pexels.com/photos/592077/pexels-photo-592077.jpeg?cs=srgb&dl=landscape-mountains-nature-592077.jpg&fm=jpg']; 
-$('body').css('background-image','url('+ backgroundImg[0]+ ')');
-});
